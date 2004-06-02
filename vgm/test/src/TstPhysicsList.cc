c5efc8ea91617b7a57e1ce24ea08f5e851022686
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2004-06-02 20:31:20 +0000

    Initial revision

---
+++ vgm/test/src/TstPhysicsList.cc
@@ -0,0 +1,58 @@
+// $Id$
+//
+// Class GTstPhysicsList
+// --------------------
+// The test physics list - defines only geantino and transportation
+// process
+//
+// Author: Ivana Hrivnacova; IPN Orsay
+
+
+#include "TstPhysicsList.hh"
+#include "G4ParticleTypes.hh"
+
+
+//_____________________________________________________________________________
+TstPhysicsList::TstPhysicsList() 
+  : G4VUserPhysicsList()
+{
+//
+}
+
+//_____________________________________________________________________________
+TstPhysicsList::~TstPhysicsList()
+{
+//
+}
+
+//_____________________________________________________________________________
+void TstPhysicsList::ConstructParticle() 
+{
+// Construct geantino only
+// ---
+
+  G4Geantino::GeantinoDefinition();
+}
+
+//_____________________________________________________________________________
+void TstPhysicsList::ConstructProcess()
+{
+// Define transportation process
+// ---
+
+  AddTransportation();
+}
+
+//_____________________________________________________________________________
+void TstPhysicsList::SetCuts()
+{
+  // Suppress error messages even in case e/gamma/proton do not exist            
+  G4int temp = GetVerboseLevel();                                                SetVerboseLevel(0);                                                           
+  //  " G4VUserPhysicsList::SetCutsWithDefault" method sets 
+  //   the default cut value for all particle types 
+  SetCutsWithDefault();   
+
+  // Retrieve verbose level
+  SetVerboseLevel(temp);  
+}
+
