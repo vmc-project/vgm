99be2042bd4aad9446329f0e5d5fb7e28dce6308
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2004-06-18 15:32:12 +0000

    Initial revision

---
+++ test/src/TstVGeometry.cc
@@ -0,0 +1,43 @@
+// $Id$
+//
+// Class TstVGeometry
+// ---------------
+// The test for solids construction
+//
+// Author: Ivana Hrivnacova; IPN Orsay
+
+#include <iostream>
+
+#include "TstVGeometry.hh"
+
+//_____________________________________________________________________________
+TstVGeometry::TstVGeometry()
+{
+//
+}  
+
+//_____________________________________________________________________________
+TstVGeometry::~TstVGeometry()
+{
+//
+}
+
+//_____________________________________________________________________________
+void* TstVGeometry::TestBooleanSolids4()
+{
+//
+  std::cerr << "TstVGeometry::TestBooleanSolids4 is not implemented." 
+            << std::endl;
+  exit(1);
+}  
+  
+    
+
+//_____________________________________________________________________________
+void* TstVGeometry::TestBooleanSolids5()
+{
+//
+  std::cerr << "TstVGeometry::TestBooleanSolids5 is not implemented." 
+            << std::endl;
+  exit(1);
+}  
