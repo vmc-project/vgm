90383e83481ff53f2934cfacb7c73035b01c9717
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2004-06-18 14:45:21 +0000

    Initial revision

---
+++ test/include/TstVGeometry.hh
@@ -0,0 +1,32 @@
+// $Id$
+//
+// Class TstVGeometry
+// --------------------------
+// The interface to test geometry builders
+//
+// Author: Ivana Hrivnacova; IPN Orsay
+
+
+#ifndef TST_V_GEOMETRY_H
+#define TST_V_GEOMETRY_H
+
+class TstVGeometry 
+{
+  public:
+    TstVGeometry();
+    virtual ~TstVGeometry();
+
+    virtual void  DefineMaterials() = 0;
+    
+    virtual void* TestSolids(bool fullPhi = true) = 0;
+    virtual void* TestPlacements() = 0;
+    virtual void* TestReflections(bool fullPhi = true) = 0;
+    virtual void* TestBooleanSolids1() = 0;
+    virtual void* TestBooleanSolids2() = 0;
+    virtual void* TestBooleanSolids3() = 0;
+    virtual void* TestBooleanSolids4();
+    virtual void* TestBooleanSolids5();
+};
+
+#endif //TST_V_GEOMETRY_BUILDER_H
+
