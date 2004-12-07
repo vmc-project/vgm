3a1d0f9eefeb1ef5de426f17c65dcea6965b90f9
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2004-12-07 19:07:08 +0000

    Initial revision

---
+++ packages/VGM/include/VGM/materials/IElement.h
@@ -0,0 +1,33 @@
+// $Id$
+//
+// Class IElement
+// ---------------
+// The VGM interface to elements.
+//
+// Author: Ivana Hrivnacova; IPN Orsay
+
+#ifndef VGM_I_ELEMENT_H
+#define VGM_I_ELEMENT_H
+
+#include <string>
+#include <iostream>
+
+namespace VGM {
+  class IElement
+  {
+    public:
+      virtual ~IElement() {}
+    
+      // methods
+      virtual std::string Name() const = 0;
+      virtual std::string Symbol() const = 0;
+
+      virtual double  Z() const = 0;     
+      virtual double  N() const = 0;     
+      virtual double  A() const = 0; 
+  };       
+}; 
+
+std::ostream& operator << (std::ostream& out, const VGM::IElement& element);
+
+#endif //VGM_I_ELEMENT_H
