d2b97c163faf6712350cd72d8059cd2103ab1d3f
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2004-12-09 08:48:30 +0000

    Initial revision

---
+++ vgm/packages/Geant4GM/include/Geant4GM/materials/Element.h
@@ -0,0 +1,49 @@
+// $Id$
+//
+// Class Element
+// ---------------
+// VGM implementation for Geant4 element.
+//
+// Author: Ivana Hrivnacova; IPN Orsay
+
+#ifndef GEANT4_GM_ELEMENT_H
+#define GEANT4_GM_ELEMENT_H
+
+#include <string>
+
+#include "G4Element.hh"
+
+#include "VGM/materials/IElement.h"
+
+namespace Geant4GM {
+
+  class Element : public virtual VGM::IElement
+  {
+    public:
+      Element(const std::string& name, 
+              const std::string& symbol,      
+              double z, double a); 
+      Element(G4Element* element);		
+      virtual ~Element();
+    
+      // methods
+      virtual std::string Name() const;
+      virtual std::string Symbol() const;
+
+      virtual double  Z() const;     
+      virtual double  N() const;     
+      virtual double  A() const;     
+
+    protected:  
+      Element() : VGM::IElement() {}  
+      Element(const Element& rhs) : VGM::IElement(rhs) {}
+    
+    private:
+      void CheckIndex(int iel) const;
+  
+      G4Element*  fElement;  
+  };
+
+}
+
+#endif //GEANT4_GM_ELEMENT_H
---
+++ vgm/packages/Geant4GM/include/Geant4GM/materials/ElementMap.h
@@ -0,0 +1,59 @@
+// $Id$
+//
+// Class ElementMap
+// ------------------
+// The map between VGM and Geant4 elements.
+//
+// Author: Ivana Hrivnacova; IPN Orsay
+
+#ifndef GEANT4_GM_ELEMENT_MAP_H
+#define GEANT4_GM_ELEMENT_MAP_H
+
+#include <map>
+
+#include "G4Element.hh"
+
+#include "VGM/materials/IElement.h"
+
+namespace Geant4GM {
+
+  class ElementMap;
+
+  class ElementMap
+  {
+    public:
+      typedef std::map<VGM::IElement*, G4Element*>  G4ElementMap;
+      typedef G4ElementMap::const_iterator          G4ElementMapCIterator;
+
+      typedef std::map<G4Element*, VGM::IElement*>  VgmElementMap;
+      typedef VgmElementMap::const_iterator         VgmElementMapCIterator;
+
+    public:
+      ElementMap();
+      virtual ~ElementMap();
+
+      // static access methods
+      static ElementMap* Instance(); 
+
+      // methods
+      void  AddElement(VGM::IElement*, G4Element*); 
+      void  Print() const;
+  
+      // get methods
+      G4Element*     GetElement(VGM::IElement* iElement) const;
+      VGM::IElement* GetElement(G4Element* g4Element) const;
+
+    private:
+      ElementMap(const ElementMap&) {}
+
+      // static data members
+      static ElementMap*  fgInstance;
+  
+      // data members
+      G4ElementMap   fG4Elements;
+      VgmElementMap  fVgmElements;
+  };
+
+}
+
+#endif //GEANT4_GM_ELEMENT_MAP_H
