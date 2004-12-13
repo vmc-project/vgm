04e8e4bcca148f33be19f8d3e7825b8e84bf9fb8
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2004-12-13 10:50:27 +0000

    Initial revision

---
+++ examples/E04/rootgeom.cxx
@@ -0,0 +1,42 @@
+#include "RootGM/volumes/Factory.h"
+#include "XmlVGM/AGDDExporter.h"
+#include "XmlVGM/GDMLExporter.h"
+
+#include "TApplication.h"
+#include "TROOT.h"
+#include "TInterpreter.h"
+#include "TGeoManager.h"
+
+int main(int argc,char** argv)
+{
+  TApplication theApp("otherwise it does not work", &argc, argv);
+
+  // Create Root geometry by running Root macro
+  //
+  gROOT->LoadMacro("rootgeom.C");
+  gInterpreter->ProcessLine("rootgeom()");
+  
+  // Draw ROOT geometry
+  //
+  gGeoManager->SetVisLevel(4);
+  gGeoManager->GetTopVolume()->Draw();
+
+  // Import Root geometry to VGM
+  //
+  RootGM::Factory factory;
+  factory.Import(gGeoManager->GetTopVolume());
+  
+  // Export geometry to AGDD
+  //
+  XmlVGM::AGDDExporter xmlExporter1(&factory);
+  xmlExporter1.GenerateXMLGeometry();
+
+  // Export geometry to AGDD
+  //
+  XmlVGM::GDMLExporter xmlExporter2(&factory);
+  xmlExporter2.GenerateXMLGeometry();
+
+  theApp.Run();
+
+  return 0;
+}  
