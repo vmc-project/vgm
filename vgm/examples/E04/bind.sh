7ea2112115c33aae0875a66d0433fab02c06a75d
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2004-12-17 13:51:49 +0000

    Added -lClhepVGM lib

--- vgm/examples/E04/bind.sh
+++ vgm/examples/E04/bind.sh
@@ -6,7 +6,7 @@ g++ -I$ROOTSYS/include \
     -I$VGM_INSTALL/packages/RootGM/include \
     -I$VGM_INSTALL/packages/XmlVGM/include \
     -I$CLHEP_BASE_DIR/include \
-    -L$VGM_INSTALL/lib/$VGM_SYSTEM -lRootGM -lBaseVGM -lXmlVGM \
+    -L$VGM_INSTALL/lib/$VGM_SYSTEM -lRootGM -lBaseVGM -lXmlVGM -lClhepVGM \
     -L$CLHEP_BASE_DIR/lib -lCLHEP \
     `root-config --glibs` -lGeomPainter -lGeom rootgeom.cxx \
     -o rootgeom
