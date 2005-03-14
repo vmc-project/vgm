47eb32908dfd8704ec1964c511ce503f94263b25
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2005-03-14 14:47:38 +0000

    configuration files for Doxygen

---
+++ packages/Doxymodules.h
@@ -0,0 +1,114 @@
+// $Id$
+// The class categories definitions for Doxygen
+
+/** @defgroup VGM_common VGM common
+ *  Category VGM common
+ *  @{
+ */
+/** @} */
+
+/** @defgroup VGM_materials VGM materials
+ *  Category VGM materials
+ *  @{
+ */
+/** @} */
+
+/** @defgroup VGM_solids VGM solids
+ *  Category VGM solids
+ *  @{
+ */
+/** @} */
+
+/** @defgroup VGM_volumes VGM volumes
+ *  Category VGM volumes
+ *  @{
+ */
+/** @} */
+
+/** @defgroup BaseVGM_common BaseVGM common
+ *  Category BaseVGM common
+ *  @{
+ */
+/** @} */
+
+/** @defgroup BaseVGM_materials BaseVGM materials
+ *  Category BaseVGM materials
+ *  @{
+ */
+/** @} */
+
+/** @defgroup BaseVGM_solids BaseVGM solids
+ *  Category BaseVGM solids
+ *  @{
+ */
+/** @} */
+
+/** @defgroup BaseVGM_volumes BaseVGM volumes
+ *  Category BaseVGM volumes
+ *  @{
+ */
+/** @} */
+
+/** @defgroup Geant4GM_common Geant4GM common
+ *  Category Geant4GM common
+ *  @{
+ */
+/** @} */
+
+/** @defgroup Geant4GM_materials Geant4GM materials
+ *  Category Geant4GM materials
+ *  @{
+ */
+/** @} */
+
+/** @defgroup Geant4GM_solids Geant4GM solids
+ *  Category Geant4GM solids
+ *  @{
+ */
+/** @} */
+
+/** @defgroup Geant4GM_volumes Geant4GM volumes
+ *  Category Geant4GM volumes
+ *  @{
+ */
+/** @} */
+
+/** @defgroup RootGM_common RootGM common
+ *  Category RootGM common
+ *  @{
+ */
+/** @} */
+
+/** @defgroup RootGM_materials RootGM materials
+ *  Category RootGM materials
+ *  @{
+ */
+/** @} */
+
+/** @defgroup RootGM_solids RootGM solids
+ *  Category RootGM solids
+ *  @{
+ */
+/** @} */
+
+/** @defgroup RootGM_volumes RootGM volumes
+ *  Category RootGM volumes
+ *  @{
+ */
+/** @} */
+
+/** @defgroup ClhepVGM ClhepVGM
+ *  Category ClhepVGM
+ *  @{
+ */
+/** @} */
+
+/** @defgroup XmlVGM XmlVGM
+ *  Category XmlVGM
+ *  @{
+ */
+/** @} */
+
+
+
+
