905bebff2af39d56db3c01fcb090c5b7e93ddf62
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2006-06-30 16:24:17 +0000

    Added new ctubs (cut tube) solid

---
+++ packages/BaseVGM/source/solids/VCtubs.cxx
@@ -0,0 +1,44 @@
+// $Id$
+//
+// Class VCtubs
+// ---------------
+// The ABC for cut tubs solids.
+//
+// Author: Ivana Hrivnacova; IPN Orsay
+
+#include "BaseVGM/solids/VCtubs.h"
+
+//_____________________________________________________________________________
+std::ostream& operator<<(std::ostream& out, const VGM::ICtubs& ctubs)
+{
+  const VGM::ISolid& ctubsSolid = ctubs;
+  out << ctubsSolid;
+  return out; 
+}
+
+//_____________________________________________________________________________
+BaseVGM::VCtubs::VCtubs()
+  : VGM::ICtubs() 
+{
+/// Default constructor  
+}
+
+//_____________________________________________________________________________
+BaseVGM::VCtubs::~VCtubs() {
+//
+}
+
+//_____________________________________________________________________________
+std::ostream& BaseVGM::VCtubs::Put(std::ostream& out) const
+{
+  out << "  rin = "    << InnerRadius()  << "mm" 
+      << "  rout = "   << OuterRadius()  << "mm" 
+      << "  hz = "     << ZHalfLength()  << "mm" 
+      << "  sphi = "   << StartPhi()     << "deg" 
+      << "  dphi = "   << DeltaPhi()     << "deg"
+      << "  nlow = ("  << NxLow() << ", " << NyLow() << ", " << NzLow() << ")"
+      << "  nhigh = (" << NxHigh() << ", " << NyHigh() << ", " << NzHigh() << ")";
+      
+  return out; 
+}
+
