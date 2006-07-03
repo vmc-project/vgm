bc7c5f6a26291331a29e33ff96ab03f665434098
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2006-07-03 14:45:49 +0000

    Updated with new Geant4 version 8.1

--- vgm/examples/E02/ExN03DetectorConstruction.hh
+++ vgm/examples/E02/ExN03DetectorConstruction.hh
@@ -1,30 +1,32 @@
 //
 // ********************************************************************
-// * DISCLAIMER                                                       *
+// * License and Disclaimer                                           *
 // *                                                                  *
-// * The following disclaimer summarizes all the specific disclaimers *
-// * of contributors to this software. The specific disclaimers,which *
-// * govern, are listed with their locations in:                      *
-// *   http://cern.ch/geant4/license                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
 // *                                                                  *
 // * Neither the authors of this software system, nor their employing *
 // * institutes,nor the agencies providing financial support for this *
 // * work  make  any representation or  warranty, express or implied, *
 // * regarding  this  software system or assume any liability for its *
-// * use.                                                             *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
 // *                                                                  *
-// * This  code  implementation is the  intellectual property  of the *
-// * GEANT4 collaboration.                                            *
-// * By copying,  distributing  or modifying the Program (or any work *
-// * based  on  the Program)  you indicate  your  acceptance of  this *
-// * statement, and all its terms.                                    *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
 //
-// Id: ExN03DetectorConstruction.hh,v 1.6 2003/09/15 15:38:14 maire Exp 
-// GEANT4 tag Name: geant4-06-00-patch-01 
-//
 // $Id$
+// GEANT4 tag $Name$
+//
 // 
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -48,11 +50,10 @@ class ExN03DetectorConstruction : public G4VUserDetectorConstruction
    ~ExN03DetectorConstruction();
 
   public:
-     
      G4VPhysicalVolume* Construct();
      const G4LogicalVolume* GetAbsorber()   {return logicAbsorber;};
      const G4LogicalVolume* GetGap()        {return logicGap;};
-
+                 
   private:
      G4LogicalVolume*  FindVolume(const G4String& name) const;
 
--- vgm/examples/E03/ExN03DetectorConstruction.cc
+++ vgm/examples/E03/ExN03DetectorConstruction.cc
@@ -1,30 +1,32 @@
 //
 // ********************************************************************
-// * DISCLAIMER                                                       *
+// * License and Disclaimer                                           *
 // *                                                                  *
-// * The following disclaimer summarizes all the specific disclaimers *
-// * of contributors to this software. The specific disclaimers,which *
-// * govern, are listed with their locations in:                      *
-// *   http://cern.ch/geant4/license                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
 // *                                                                  *
 // * Neither the authors of this software system, nor their employing *
 // * institutes,nor the agencies providing financial support for this *
 // * work  make  any representation or  warranty, express or implied, *
 // * regarding  this  software system or assume any liability for its *
-// * use.                                                             *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
 // *                                                                  *
-// * This  code  implementation is the  intellectual property  of the *
-// * GEANT4 collaboration.                                            *
-// * By copying,  distributing  or modifying the Program (or any work *
-// * based  on  the Program)  you indicate  your  acceptance of  this *
-// * statement, and all its terms.                                    *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
 //
-// Id: ExN03DetectorConstruction.cc,v 1.22 2005/11/14 16:17:20 maire Exp 
-// GEANT4 tag Name: geant4-08-00 
-//
 // $Id$
+// GEANT4 tag $Name$
+//
 // 
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -53,7 +55,6 @@
 #include "XmlVGM/AGDDExporter.h"
 #include "XmlVGM/GDMLExporter.h"
 // end VGM demo
-
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -320,8 +321,8 @@ G4VPhysicalVolume* ExN03DetectorConstruction::ConstructCalorimeter()
 		     // however	there were not supposed to get clash
 		     // from material x volume names
 		     // TO DO : to take care of this in future 		 
-
-     physiAbsorber = new G4PVPlacement(0,		   //no rotation
+      			                  
+      physiAbsorber = new G4PVPlacement(0,		   //no rotation
       		    G4ThreeVector(-GapThickness/2,0.,0.),  //its position
                                         logicAbsorber,     //its logical volume		    
                                         AbsorberMaterial->GetName(), //its name
@@ -344,6 +345,7 @@ G4VPhysicalVolume* ExN03DetectorConstruction::ConstructCalorimeter()
       				     GapMaterial,
       				     //GapMaterial->GetName());
       				     "Gap");
+
  		     // The name of logical volume has been changed
 		     // to be different from the name of material
       				     
@@ -389,7 +391,6 @@ G4VPhysicalVolume* ExN03DetectorConstruction::ConstructCalorimeter()
   logicGap->SetVisAttributes(atb);}
   */
 
-
   // ---------------------------------------------------------------------------
   // VGM demo 
   //
@@ -420,6 +421,7 @@ G4VPhysicalVolume* ExN03DetectorConstruction::ConstructCalorimeter()
   //
   // end VGM demo
   //---------------------------------------------------------------------------
+
 
 
   //
