d38ea3b65c2096ff564e0a7eda380e5c5018ee64
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2006-07-05 10:52:03 +0000

    Definition of <math> constant M_PI not available on Win32

---
+++ vgm/packages/VGM/include/VGM/common/Math.h
@@ -0,0 +1,17 @@
+// $Id$
+/// \ingroup VGM_common
+///
+/// Definition of <math> constant M_PI not available on Win32
+///
+/// Author: Ivana Hrivnacova; IPN Orsay
+
+#ifndef VGM_MATH_H
+#define VGM_MATH_H
+
+#include <math.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#endif //VGM_MATH_H
