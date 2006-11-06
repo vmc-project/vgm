80362067f789431ce3b4c0898f1531559aae6054
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2006-11-06 15:01:28 +0000

    Updated for new material functions: now printing also material state, temperature and pressure

--- packages/BaseVGM/source/materials/material.cxx
+++ packages/BaseVGM/source/materials/material.cxx
@@ -8,13 +8,27 @@
 #include "VGM/materials/IMaterial.h"
 #include "VGM/materials/IElement.h"
 
+#include <string>
+
 //_____________________________________________________________________________
 std::ostream& operator<<(std::ostream& out, const VGM::IMaterial& material)
 {
+  std::string stateName;
+  switch ( material.State() ) {
+    case VGM::kSolid:     stateName = "Solid"; break;
+    case VGM::kLiquid:    stateName = "Liquid"; break;
+    case VGM::kGas:       stateName = "Gas"; break;
+    case VGM::kUndefined:
+    default:              stateName = "Undefined"; 
+  }  
+
   out << "Material: " << "\"" << material.Name() << "\""
       << "  density " << material.Density() << " g/cm3"
       << "  radlen "  << material.RadiationLength()/10.  << " cm"
       << "  intlen "  << material.NuclearInterLength()/10. << " cm"
+      << "  state "   << "\"" << stateName << "\""
+      << "  temperature " << material.Temperature() << " kelvin"
+      << "  pressure "    << material.Pressure() << " atm"
       << std::endl;
       
   for (int i=0; i<material.NofElements(); i++) {
