95bd82b25beb65718df63b4e7747d4f51bb09a31
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2007-01-11 10:58:31 +0000

    Adding support for isotopes: - now printing also the list of isotopes if present

--- packages/BaseVGM/source/materials/element.cxx
+++ packages/BaseVGM/source/materials/element.cxx
@@ -6,6 +6,7 @@
 // Author: Ivana Hrivnacova; IPN Orsay
 
 #include "VGM/materials/IElement.h"
+#include "VGM/materials/IIsotope.h"
 
 //_____________________________________________________________________________
 std::ostream& operator<<(std::ostream& out, const VGM::IElement& element)
@@ -15,7 +16,14 @@ std::ostream& operator<<(std::ostream& out, const VGM::IElement& element)
       << "\"" << element.Symbol() << "\""
       << "  Z = "  << element.Z()
       << "  N = "  << element.N()
-      << "  A = "  << element.A() << " g/mole";  
+      << "  A = "  << element.A() << " g/mole";
+
+  for ( int i=0; i<element.NofIsotopes(); i++ ) {
+    if ( i == 0 ) out << std::endl;
+    out << "     "
+        << i << "th isotope: " << *element.Isotope(i);
+    if ( i<element.NofIsotopes()-1 ) out << std::endl;    
+  }       
     
   return out;    
 }
