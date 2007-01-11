a21b0a85fba488e053f4af3a04e1f39b1fa9ad26
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2007-01-11 10:58:04 +0000

    Adding support for isotopes: added fIsotopes data member of  VGM::IsotopeStore type and related functions analogical to Element

--- vgm/packages/BaseVGM/include/BaseVGM/materials/VMaterialFactory.h
+++ vgm/packages/BaseVGM/include/BaseVGM/materials/VMaterialFactory.h
@@ -33,10 +33,12 @@ namespace BaseVGM {
       //
       virtual std::string Name() const;
 
+      virtual const VGM::IsotopeStore&   Isotopes() const;	
       virtual const VGM::ElementStore&   Elements() const;	
       virtual const VGM::MaterialStore&  Materials() const;	
       virtual const VGM::MediumStore&    Media() const;	
 
+      virtual const VGM::IIsotope*   Isotope(const std::string& name) const;	
       virtual const VGM::IElement*   Element(const std::string& name) const;	
       virtual const VGM::IMaterial*  Material(const std::string& name) const;	
       virtual const VGM::IMedium*    Medium(const std::string& name) const;	
@@ -47,6 +49,7 @@ namespace BaseVGM {
  
       // listings
       //
+      virtual void  PrintIsotopes() const;			       
       virtual void  PrintElements() const;			       
       virtual void  PrintMaterials() const;
       virtual void  PrintMedia() const;
@@ -60,17 +63,22 @@ namespace BaseVGM {
       VMaterialFactory();
       VMaterialFactory(const VMaterialFactory& rhs); 
 
+      virtual VGM::IsotopeStore&    IsotopeStore();			       
       virtual VGM::ElementStore&    ElementStore();			       
       virtual VGM::MaterialStore&   MaterialStore();			       
       virtual VGM::MediumStore&     MediumStore();			       
 
     private:
       // types
+      typedef std::map<VGM::IIsotope*,  VGM::IIsotope*>   IsotopeMap;
       typedef std::map<VGM::IElement*,  VGM::IElement*>   ElementMap;
       typedef std::map<VGM::IMaterial*, VGM::IMaterial*>  MaterialMap;
 
-      VGM::IElement*  ExportElement(VGM::IElement* element, 
+      VGM::IIsotope*  ExportIsotope(VGM::IIsotope* isotope, 
                               VGM::IMaterialFactory* factory) const;
+      VGM::IElement*  ExportElement(VGM::IElement* element, 
+                              VGM::IMaterialFactory* factory,
+			      IsotopeMap* map) const;
       VGM::IMaterial* ExportMaterial(VGM::IMaterial* material, 
                               VGM::IMaterialFactory* factory,
 			      ElementMap* map) const;
@@ -82,7 +90,9 @@ namespace BaseVGM {
                               VGM::IMaterialFactory* factory,
 			      MaterialMap* map) const;
 
-      ElementMap*  ExportElements(VGM::IMaterialFactory* factory) const;			       
+      IsotopeMap*  ExportIsotopes(VGM::IMaterialFactory* factory) const;			       
+      ElementMap*  ExportElements(VGM::IMaterialFactory* factory,
+                                IsotopeMap* map) const;			       
       MaterialMap* ExportMaterials(VGM::IMaterialFactory* factory, 
                                 ElementMap* map) const;			       
       void         ExportMedia(VGM::IMaterialFactory* factory, 
@@ -93,6 +103,7 @@ namespace BaseVGM {
       // data members
       int                 fDebug;
       std::string         fName;
+      VGM::IsotopeStore   fIsotopes;
       VGM::ElementStore   fElements;
       VGM::MaterialStore  fMaterials;
       VGM::MediumStore    fMedia;
@@ -106,6 +117,10 @@ inline std::string
 BaseVGM::VMaterialFactory::Name() const
 { return fName; }
 
+inline const VGM::IsotopeStore& 
+BaseVGM::VMaterialFactory::Isotopes() const
+{ return fIsotopes; }
+
 inline const VGM::ElementStore& 
 BaseVGM::VMaterialFactory::Elements() const
 { return fElements; }
@@ -118,6 +133,13 @@ inline const VGM::MediumStore&
 BaseVGM::VMaterialFactory::Media() const
 { return fMedia; }
 
+inline VGM::IsotopeStore& 
+BaseVGM::VMaterialFactory::IsotopeStore()
+{  
+  /// Return the store of isotopes (non const)
+  return fIsotopes; 
+}
+			       
 inline VGM::ElementStore& 
 BaseVGM::VMaterialFactory::ElementStore()
 {  
