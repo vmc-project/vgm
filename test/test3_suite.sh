1e4613150ba1706bfa806954646b6ee62da83ba7
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2007-08-06 11:09:44 +0000

    Adding AGDD in test

--- test/test3_suite.sh
+++ test/test3_suite.sh
@@ -25,11 +25,11 @@ then
   mkdir -p $OUTDIR 
 fi
 
-for inputType in VGM Geant4 Root
+for inputType in VGM AGDD Geant4 Root
 # Use the line below for performance tests (G4 navigator)/G4Root navigator)
 # for inputType in Geant4 Root
 do
-  for inputFactory in Geant4 Root
+  for inputFactory in AGDD Geant4 Root
   do
     for outputFactory in None Geant4
     # Use the line below for performance tests (G4 navigator)/G4Root navigator)
@@ -46,20 +46,44 @@ do
 	then
 	  DUMMY=0
 	else 
-  	  if [ "$inputFactory" = "$outputFactory" ]
+ 	  if [ $inputType = "VGM" -a "$inputFactory" = "AGDD" ]
 	  then
-  	    DUMMY=0
+	    DUMMY=0
 	  else 
-	    NAVIG=""
-	    if [ $inputFactory = "Root" -a "$outputFactory" = "None" ]
+  	    if [ "$inputFactory" = "$outputFactory" ]
 	    then
-              NAVIG=rootNavig
-	    fi        
-            echo "Testing configuration: $inputType $inputFactory $outputFactory $selectedTest $NAVIG"
-	    vgm_test $inputType $inputFactory $outputFactory $NOXML $selectedTest $NOVIS run $NAVIG \
-	      >& $OUTDIR/"$inputType.$inputFactory.$outputFactory.$selectedTest.out" 
+  	      DUMMY=0
+  	    else 
+	      NAVIG=""
+	      if [ $inputFactory = "Root" -a "$outputFactory" = "None" ]
+	      then
+                NAVIG=rootNavig
+	      fi        
+              echo "Testing configuration: $inputType $inputFactory $outputFactory $selectedTest $NAVIG"
+	      vgm_test $inputType $inputFactory $outputFactory $NOXML $selectedTest $NOVIS run $NAVIG \
+	        >& $OUTDIR/"$inputType.$inputFactory.$outputFactory.$selectedTest.out" 
+            fi    
           fi
         fi
+      done 	
+    done
+  done  
+done  
+
+for inputType in AGDD
+do
+  for inputFactory in AGDD
+  do
+    for outputFactory in Root
+    # Use the line below for performance tests (G4 navigator)/G4Root navigator)
+    # for outputFactory in None
+    do
+      for selectedTest in Solids Placements Reflections Assemblies1 Assemblies2 BooleanSolids1 BooleanSolids2
+      do
+ 	NAVIG=rootNavig
+        echo "Testing configuration: $inputType $inputFactory $outputFactory $selectedTest $NAVIG"
+	vgm_test $inputType $inputFactory $outputFactory $NOXML $selectedTest $NOVIS run $NAVIG \
+	  >& $OUTDIR/"$inputType.$inputFactory.$outputFactory.$selectedTest.out" 
       done 	
     done
   done  
