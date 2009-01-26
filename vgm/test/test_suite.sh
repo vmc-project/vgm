7832a449d846be9d0d1bebec5d2f78f40e36ba66
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2009-01-26 15:20:54 +0000

    In test: Adding script for running all test suites at once.

---
+++ vgm/test/test_suite.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+# $Id$
+
+# -----------------------------------------------------------------------
+# The test script of the Virtual Geometry Model
+# Copyright (C) 2007, Ivana Hrivnacova               
+# All rights reserved. 
+#           
+# For the licensing terms see vgm/LICENSE.
+# Contact: ivana@ipno.in2p3.fr
+# -----------------------------------------------------------------------
+
+./test1_suite.sh
+./test2_suite.sh
+./test3_suite.sh
