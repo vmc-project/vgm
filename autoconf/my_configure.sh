1573796f8900ceb0d29c5e216b404fa1168e69f6
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2009-02-04 15:23:26 +0000

    Fixes in building system: In GNUmakefiles: - using systematically root-config instead of ROOTSYS; - updated OGLLIBS in sys/Darwin.gmk In autoconf: - adding back configure script; - updated Makefile.ac files for newly added classes

---
+++ autoconf/my_configure.sh
@@ -0,0 +1,23 @@
+# $Id$
+
+# -----------------------------------------------------------------------
+# The script for the Virtual Geometry Model test
+# Copyright (C) 2007, Ivana Hrivnacova               
+# All rights reserved. 
+#           
+# For the licensing terms see vgm/LICENSE.
+# Contact: ivana@ipno.in2p3.fr
+# -----------------------------------------------------------------------
+
+# An example of configure options for building VGM
+# with all supported packages
+#
+# by I. Hrivnacova, IPN Orsay
+
+
+./configure --prefix=/work/projects/vgm/autoconf/vgm_built \
+            --with-xerces=/usr \
+            --with-clhep=/work/packages/clhep/2.0.4.2 \
+            --with-geant4=/work/projects/vgm/new/geant4 \
+            --with-root=/work/projects/vgm/new/root \
+            --with-libagdd=/work/projects/vgm/new/agdd
