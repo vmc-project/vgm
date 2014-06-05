d9e0a24adc8be9c0bff922738044039e036bb9a6
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2014-06-05 11:11:23 +0000

    New version of examples based on Geant4 10.00.p01; improved CMake build: use VGM config file, now available

---
+++ examples/E02/src/B4DetectorConstruction.cc
@@ -0,0 +1,180 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: B4DetectorConstruction.cc 77601 2013-11-26 17:08:44Z gcosmo $
+// 
+/// \file B4DetectorConstruction.cc
+/// \brief Implementation of the B4DetectorConstruction class
+
+#include "B4DetectorConstruction.hh"
+
+#include "G4Material.hh"
+#include "G4NistManager.hh"
+
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4PVReplica.hh"
+#include "G4GlobalMagFieldMessenger.hh"
+#include "G4AutoDelete.hh"
+
+#include "G4GeometryManager.hh"
+#include "G4PhysicalVolumeStore.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4SolidStore.hh"
+
+#include "G4VisAttributes.hh"
+#include "G4Colour.hh"
+
+#include "G4PhysicalConstants.hh"
+
+// VGM demo
+#include "Geant4GM/volumes/Factory.h"
+#include "RootGM/volumes/Factory.h"
+#include "TGeoManager.h"
+// end VGM demo
+
+#include "G4SystemOfUnits.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4ThreadLocal 
+G4GlobalMagFieldMessenger* B4DetectorConstruction::fMagFieldMessenger = 0; 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B4DetectorConstruction::B4DetectorConstruction()
+ : G4VUserDetectorConstruction(),
+   fAbsorberPV(0),
+   fGapPV(0),
+   fCheckOverlaps(true)
+{
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+  // ---------------------------------------------------------------------------
+  // VGM demo 
+  //
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4LogicalVolume* FindVolume(const G4String& name)
+{
+  G4LogicalVolumeStore* lvStore = G4LogicalVolumeStore::GetInstance();
+  
+  for (G4int i=0; i<G4int(lvStore->size()); i++) {
+    G4LogicalVolume* lv = (*lvStore)[i];
+    if (lv->GetName() == name) return lv;
+  }
+
+  G4String text = "ExN03DetectorConstruction:: FindVolume:\n"; 
+  text = text + "    Logical volume " + name + " does not exist.";
+  std::cerr << text << G4endl;
+  return 0;
+}  	       	         
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B4DetectorConstruction::~B4DetectorConstruction()
+{ 
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* B4DetectorConstruction::Construct()
+{
+  // 
+  // Import geometry from Root
+  //
+
+  // Import geometry from the root file
+  new TGeoManager("B4a", "Geant4 basic example B4a");
+  gGeoManager->Import("geometry.root");
+
+  // Import geometry from Root to VGM
+  RootGM::Factory rtFactory;
+  rtFactory.SetDebug(1);
+  rtFactory.Import(gGeoManager->GetTopNode());
+
+  // Export VGM geometry to Geant4
+  //
+  Geant4GM::Factory g4Factory;
+  g4Factory.SetDebug(1);
+  rtFactory.Export(&g4Factory);
+  G4VPhysicalVolume* world = g4Factory.World();
+
+  // Get volumes from logical volume store by name
+  G4LogicalVolume* calorLV = FindVolume("Calorimeter");
+  
+  // Visualization attributes
+  //
+  G4LogicalVolume* worldLV = world->GetLogicalVolume();
+  worldLV->SetVisAttributes (G4VisAttributes::Invisible);
+
+  G4VisAttributes* simpleBoxVisAtt= new G4VisAttributes(G4Colour(1.0,1.0,1.0));
+  simpleBoxVisAtt->SetVisibility(true);
+  if (calorLV) calorLV->SetVisAttributes(simpleBoxVisAtt);
+  
+  return world;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B4DetectorConstruction::DefineMaterials()
+{ 
+// Dummy, as materials are imported via VGM
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* B4DetectorConstruction::DefineVolumes()
+{
+// Dummy, as geometry is imported via VGM
+
+  return 0;
+}
+
+  //
+  // end VGM demo
+  //---------------------------------------------------------------------------
+
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B4DetectorConstruction::ConstructSDandField()
+{ 
+  // Create global magnetic field messenger.
+  // Uniform magnetic field is then created automatically if
+  // the field value is not zero.
+  G4ThreeVector fieldValue = G4ThreeVector();
+  fMagFieldMessenger = new G4GlobalMagFieldMessenger(fieldValue);
+  fMagFieldMessenger->SetVerboseLevel(1);
+  
+  // Register the field messenger for deleting
+  G4AutoDelete::Register(fMagFieldMessenger);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
