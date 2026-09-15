432e6a63367e8c9de7c3432c2d0a60835013f4dd
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2026-09-15 16:03:34 +0200

    Making version 5.5

--- packages/BaseVGM/include/BaseVGM/common/version.h
+++ packages/BaseVGM/include/BaseVGM/common/version.h
@@ -18,7 +18,7 @@
 ///
 /// \author Ivana Hrivnacova; IJClab Orsay
 
-#define VGM_RELEASE "5.4"
-#define VGM_RELEASE_DATE "18 December 2025"
+#define VGM_RELEASE "5.5"
+#define VGM_RELEASE_DATE "15 September 2026"
 
 #endif // BASE_VGM_VERSION_H
